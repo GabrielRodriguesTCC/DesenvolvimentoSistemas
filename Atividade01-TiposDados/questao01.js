70839162d1e1abda4ed3b04a577a4b67a0893fe3
Gabriel Rodrigues <gabrielfelipesantosro@gmail.com>
2025-03-21 14:18:52 +0000

    questao 01 (21/03/25)

---
+++ Atividade01-TiposDados/questao01.js
@@ -0,0 +1,10 @@
+let n1 = 1
+let n2 = 2
+let n3 = 3
+let n4 = 4
+let n5 = 5
+let apr = "Aprendi"
+let js = "JavaScript"
+console.log(n5, ", ", n4, ", ", n3, " ", n2, " ", n1, "... ",apr, " ", js);
+
+
